fc4606a6cfbcab7f3084ac64cd3a38e69fa22448
Shelter <pradosh.balaji@gmail.com>
2023-02-20 23:35:24 +0530

    Responsive changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -25,35 +25,59 @@ function Card(props) {
         <div className="card-image">
           <img src={avatar_url} alt="avatar" />
         </div>
-      </div>
-      <div className="main-right">
         <div className="card-content">
           <h1 className="name">{name}</h1>
           <p className="username" style={{ color: "#0863ca" }}>
             @{login}
           </p>
-          <p className="bio">{bio===null ? 'Bio not available':bio}</p>
+          <p className="bio">{bio === null ? "Bio not available" : bio}</p>
         </div>
-        <SmallCard following={following} followers={followers} repos={public_repos}/>
+      </div>
+      <div className="main-right">
+        <SmallCard
+          following={following}
+          followers={followers}
+          repos={public_repos}
+        />
         <div className="details">
           <div className="left">
-            <div className={location === null ? 'location not-available inside' : 'location inside'}>
+            <div
+              className={
+                location === null
+                  ? "location not-available inside"
+                  : "location inside"
+              }
+            >
               <img src={location_img} alt="location" />
-              <h3>{location===null ? 'Not available':location}</h3>
+              <h3>{location === null ? "Not available" : location}</h3>
             </div>
-            <div className={twitter_username === null ? 'twitter not-available inside' : 'twitter inside'}>
+            <div
+              className={
+                twitter_username === null
+                  ? "twitter not-available inside"
+                  : "twitter inside"
+              }
+            >
               <img src={twitter_img} alt="twitter" />
-              <h3>{twitter_username===null ? 'Not available':twitter_username}</h3>
+              <h3>
+                {twitter_username === null ? "Not available" : twitter_username}
+              </h3>
             </div>
           </div>
           <div className="right ">
             <div className="link inside">
               <img src={link_img} alt="link" />
-              <a href={blog}>{blog=== 0 ? "Not available" : blog}</a>
+              <a href={blog}>{blog === 0 ? "Not available" : blog}</a>
             </div>
-            <div className={company === null ? 'company not-available inside' : 'company inside'}>
+            <div
+              className={
+                company === null
+                  ? "company not-available inside"
+                  : "company inside"
+              }
+            >
               <img src={company_img} alt="company" />
-              <h3>{company===null ? 'Not available':company}</h3>
+              <h3>{company === null ? "Not available" : company}</h3>
             </div>
           </div>
         </div>
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import search_img from '../assets/icon-search.svg';
 import './searchbar.css';
 function SearchBar(props){
     const [term,setTerm]=useState('');
@@ -14,6 +15,7 @@ function SearchBar(props){
         
         <div>
             <form >
+                <img src={search_img} className="search_img" alt="search" />
                 <input type="text" value={term} placeholder="Search Github Username..."  onChange={handleChange}/>
                 <button className="search" onClick={handleSubmit}>Search</button>
             </form>
